82c481fbce166e4a5e13f036e41734e332229281
SercanGuler <sercanguler@live.com>
2016-06-23 17:00:32 +0300

    Add project files.

---
+++ SingnalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SingnalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+
+        public void Send(string name,string message)
+        {
+            Clients.All.sendMessage(name,message);
+        }
+    }
+}
